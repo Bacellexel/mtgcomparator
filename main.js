68863bb2b167fa894d673fbc690e01b79bd49dc6
Sebastien <snuzfr@gmail.com>
2021-12-30 13:28:34 -0500

    fixing new getdata func

--- main.js
+++ main.js
@@ -63,7 +63,7 @@ app.post('/', async (req, res) => {
     let f2fPriceArray = await getData(page, f2fPrice);
     let f2fNameArray = await getData(page, f2fName);
     let f2fEditionArray = await getData(page, f2fEdition)
-    let f2fImageArray = await getAttributeData(page, f2fImage, "src");
+    let f2fImageArray = await getAttributeData(page, f2fImage);
 
     let imgPriceArray = await getData(page2, imgPrice);
     let imgNameArray = await getData(page2, imgName);
@@ -112,11 +112,11 @@ async function getData(page, selector){
     return valueArray;
 }
 
-async function getAttributeData(page, selector, attribute){
+async function getAttributeData(page, selector){
     let valueArray = await page.evaluate((selector) =>{
         let values = Array.from(document.querySelectorAll(selector));
         let results = values.map(value => {
-            return value.getAttribute(attribute);
+            return value.getAttribute("src");
         })
         return results;
     }, selector);
